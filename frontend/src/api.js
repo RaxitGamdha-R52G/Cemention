f89bc369937bd110b5205ac562150a86e85cf7df
Raxit Gamdha <115684444+RaxitGamdha-R52G@users.noreply.github.com>
2026-02-08 22:50:51 +0530

    changes1: modification

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
+const BACKEND_URL = process.env.REACT_APP_API_URL || 'http://localhost:8000';
 export const API_BASE = `${BACKEND_URL}/api`;
 
 const api = axios.create({
